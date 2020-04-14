75e2af52d320f00509d968ea3248cdc0c7cd695e
o5shorioush <38297683+o5shorioush@users.noreply.github.com>
2020-04-14 21:27:06 +0300

    Aggregate multiple coverage results

--- src/Service/ReportService.php
+++ src/Service/ReportService.php
@@ -48,6 +48,65 @@ class ReportService
      */
     public function generateReport(CodeCoverage $coverage)
     {
+        if (!$this->config['report']['final']) {
+            if (!is_dir("coverage_data")) mkdir("coverage_data");
+            if (!is_dir("coverage_data/data")) mkdir("coverage_data/data");
+            if (!is_dir("coverage_data/test")) mkdir("coverage_data/test");
+            file_put_contents("coverage_data/data/" . rand() . rand() . ".json", json_encode($coverage->getData()));
+            file_put_contents("coverage_data/test/" . rand() . rand() . ".json", json_encode($coverage->getTests()));
+        } else {
+            $CoverageResults = [];
+            $CoverageData = [];
+            $CoverageTest = [];
+            $EmptyLines = [];
+            $num = 0;
+            
+            ///////////////////////////////////
+            $data_dir = array_diff(scandir("coverage_data/data"), array('..', '.'));
+            foreach ($data_dir as $file) {
+                $result = json_decode(file_get_contents("coverage_data/data/" . $file), true);
+                array_push($CoverageResults,$result);
+            }
+            
+            if(sizeof($CoverageResults)==1)  $CoverageData = $CoverageResults[0];
+            
+            $FileNames = array_keys($CoverageResults[0]);
+            
+            foreach($FileNames as $FileName){
+                foreach ($CoverageResults as $result) {
+                    foreach($result[$FileName] as $LineNumber => $Features){
+                        if(!isset($CoverageData[$FileName][$LineNumber])){
+                            if(!isset($CoverageData[$FileName]))
+                                $CoverageData[$FileName] = [];
+                            $combination = [];
+                            for ($i=0;$i<sizeof($CoverageResults);$i++){
+                                if(array_key_exists($LineNumber,$CoverageResults[$i][$FileName]))
+                                    $combination = $this->combine($combination,$CoverageResults[$i][$FileName][$LineNumber]);
+                                else
+                                    $combination = 3;
+                            }
+                            if($combination!=3)
+                                $CoverageData[$FileName][$LineNumber] = $combination;
+                        }
+                    }
+                }
+            }
+            
+            $test_dir = array_diff(scandir("coverage_data/test"), array('..', '.'));
+
+            foreach ($test_dir as $file) {
+
+                $arr = json_decode(file_get_contents("coverage_data/test/" . $file), true);
+                foreach ($arr as $feature => $content) {
+                    $CoverageTest[$feature] = $content;
+                }
+            }
+
+            $coverage->setData($CoverageData);
+            $coverage->setTests($CoverageTest);
+            
+        }
+
         $format = $this->config['report']['format'];
         $options = $this->config['report']['options'];
 
@@ -56,6 +115,31 @@ class ReportService
 
         if ('text' === $format) {
             print_r($output);
+        }
+    }
+    
+    public function combine($content1,$content2){
+        if(is_array($content1) && empty($content1)){
+            return $content2;
+        }
+        if($content1 == 3){
+            if(is_array($content2) && empty($content2))
+                return 3;
+            else
+                return $content2;
+        }
+        else if(is_null($content1)){
+            if((is_array($content2) && empty($content2)) || $content2 == 3)
+                return $content1;
+            else
+                return $content2;
+        }
+        else{
+            if(!$content2 || empty($content2) || $content2 == 3)
+                return $content1;
+            else
+                return array_unique(array_merge($content1,$content2));
+            
         }
     }
 }
